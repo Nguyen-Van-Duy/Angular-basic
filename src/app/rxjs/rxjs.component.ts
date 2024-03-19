0a70fba24b746cd037046e7e481dea623cb470ab
= <duynv@rikkeisoft.com>
2024-03-19 18:45:33 +0700

    update

--- src/app/rxjs/rxjs.component.ts
+++ src/app/rxjs/rxjs.component.ts
@@ -4,6 +4,8 @@ import { ServerHttpService } from '../Services/server-http.service';
 import { MatDialog } from '@angular/material/dialog';
 import { InAppFormComponent } from '../in-app-form/in-app-form.component';
 import { FormEventComponent } from './form-event/form-event.component';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject, debounce, debounceTime, delay, distinctUntilChanged, map, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -12,12 +14,40 @@ import { FormEventComponent } from './form-event/form-event.component';
 })
 export class RxjsComponent implements OnInit {
 
-  constructor(private common: CommonService, private serverService: ServerHttpService, private dialog: MatDialog){
+  search = new FormControl("");
+  formFilter!: FormGroup;
+  filter$!: BehaviorSubject<FormGroup>;
+
+  constructor(private common: CommonService, private serverService: ServerHttpService, private dialog: MatDialog, private fb: FormBuilder){
+    this.formFilter = this.fb.group({
+      name: ["", Validators.required],
+      age: ["", Validators.required]
+    })
   }
 
   ngOnInit(): void {
-    this.serverService.getListAddress().subscribe(res=>{
-      console.log(res)
+    this.filter$ = new BehaviorSubject(this.formFilter.value);
+    this.filter$.pipe(
+      // startWith(this.formFilter.value),
+      debounceTime(500),
+      map(res => {
+      console.log("this.filter$: ", res);
+    }))
+    .subscribe(res => {
+      // console.log("result: ", res);
+    })
+
+    // this.serverService.getListAddress().subscribe(res=>{
+    //   console.log(res)
+    // })
+
+    this.search.valueChanges.pipe(
+      distinctUntilChanged(),
+      map(value => console.log("value: ", value))
+    )
+    .subscribe(res => {
+      console.log("res: ", this.formFilter.value);
+
     })
   }
 
@@ -37,6 +67,11 @@ export class RxjsComponent implements OnInit {
           console.log(result);
         }
       });
+  }
+  handleChange($event: any) {
+    console.log($event);
+    this.filter$.next(this.formFilter.value)
+
   }
 
 }
